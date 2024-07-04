5bc82c1d0805beec37f1c6085dd07c5a51b7b2d4
DevKoenv <koenvanvlimmeren@gmail.com>
2024-07-04 12:04:42 +0200

    Emit log history only to newly connected clients

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -1,7 +1,6 @@
 import { createServer } from "http";
 import { Server as IOServer } from "socket.io";
 import app from "./server";
-// import { Log } from "../database/models/Log";
 import { serverConfig } from "../config";
 import { Log } from "../database";
 import { format } from "../utils/date";
@@ -17,20 +16,21 @@ io.on("connection", async (socket) => {
     order: [["date", "DESC"]],
   });
   logs.reverse().forEach((log) => {
-    io.emit("log", {
+    socket.emit("log", {
       log_type: log.level,
       message: log.message,
       log_time: format(new Date(log.date)),
     });
   });
+  socket.emit("log", { log_time: format(new Date(Date.now())), log_type: "daemon", message: "Connected to the server." });
 });
 
 process.on('unhandledRejection', (reason, promise) => {
-  io.emit('log', { log_type: 'error', message: `Unhandled Rejection at: ${promise}, reason: ${reason}` });
+  io.emit('log', { log_time: format(new Date(Date.now())), log_type: 'error', message: `Unhandled Rejection at: ${promise}, reason: ${reason}` });
 });
 
 process.on('uncaughtException', (err) => {
-  io.emit('log', { log_type: 'error', message: `Uncaught Exception: ${err.message}` });
+  io.emit('log', { log_time: format(new Date(Date.now())), log_type: 'error', message: `Uncaught Exception: ${err.message}` });
 });
 
 export { socketServer, io };
